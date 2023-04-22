496238451b1d7c104c3b5faf942184bb63ec523c
koloseki <koloseki2002@gmail.com>
2023-04-22 17:46:58 +0200

    Added a reset button , and it's now possible to win

--- script.js
+++ script.js
@@ -2,24 +2,81 @@ let gameBoard = {
     board: ["", "", "", "", "", "", "", "", ""],
 };
 
-let player = (name, symbol) => {
-    return {name, symbol};
+function Player(name, symbol){
+    this.name = name
+    this.symbol= symbol
 }
 
+
+let player1 = new Player("Player 1", "X");
+let player2 = new Player("Player 2", "O");
+
 let i = 0;
+let gameOver = false;
+
 
 function clicked(x){
-   console.log(x);
-   gameBoard.board[x] = "X";
-    console.log(gameBoard.board);
-    document.querySelector(`#button_${x}`).innerHTML = "X";
+    console.log(x);
+    let currentPlayer = i % 2 === 0 ? player1 : player2;
+
+    gameBoard.board[x] = currentPlayer.symbol;
+    document.querySelector(`#button_${x}`).innerHTML = currentPlayer.symbol;
     document.querySelector(`#button_${x}`).disabled = true;
 
-    if (i % 2 ){
-        gameBoard.board[x] = "O";
-        document.querySelector(`#button_${x}`).innerHTML = "O";
-        document.querySelector(`#button_${x}`).disabled = true;
+    let winner = checkWin(currentPlayer);
+    if (winner) {
+        console.log(`${winner.name} wygrał!`);
+        gameOver = true;
+        // dalsze akcje po wygranej
+    } else {
+        console.log("Gra nadal trwa.");
+        // dalsze akcje po każdym ruchu
     }
-    i++
+
+    if (gameOver) {
+        // zablokowanie wszystkich przycisków na planszy
+        let buttons = document.querySelectorAll(".square");
+        buttons.forEach(button => button.disabled = true);
+    }
+
+    i++;
+}
+
+
+function checkWin(currentPlayer) {
+    let symbol = currentPlayer.symbol;
+
+    // sprawdzenie wierszy
+    for (let i = 0; i < 9; i += 3) {
+        if (gameBoard.board[i] === symbol && gameBoard.board[i+1] === symbol && gameBoard.board[i+2] === symbol) {
+            return currentPlayer;
+        }
+    }
+
+    // sprawdzenie kolumn
+    for (let i = 0; i < 3; i++) {
+        if (gameBoard.board[i] === symbol && gameBoard.board[i+3] === symbol && gameBoard.board[i+6] === symbol) {
+            return currentPlayer;
+        }
+    }
+
+    // sprawdzenie przekątnych
+    if (gameBoard.board[0] === symbol && gameBoard.board[4] === symbol && gameBoard.board[8] === symbol) {
+        return currentPlayer;
+    }
+
+    if (gameBoard.board[2] === symbol && gameBoard.board[4] === symbol && gameBoard.board[6] === symbol) {
+        return currentPlayer;
+    }
+
+    return null;
+}
+
+function reset(){
+    gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+    gameOver = false;
+    let buttons = document.querySelectorAll(".square");
+    buttons.forEach(button => button.disabled = false);
+    buttons.forEach(square => {square.innerHTML = '';});
 
 }
